0ee1dda01da6f585d7ed13b4347aab6dffc1d383
JMGallego <JMGallego@zMacPro-Master.local>
2020-01-10 21:40:52 +0100

    corner cases

--- trainer/tests/test_view.py
+++ trainer/tests/test_view.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 from ..view import TrainerReciver
 
 class testTrainerView(unittest.TestCase):
@@ -19,6 +20,42 @@ class testTrainerView(unittest.TestCase):
         trainer = TrainerReciver()
         fake_data = [{"name":"aname1"}, {"name": "anothername"}]
         expected_result = [{"name":"Aname1"}, {"name": "Anothername"}]
+
+        # ACT
+        result = trainer.capitalize(fake_data) # SUT (SUBJECT UNDER TEST)
+        
+        # ASSERT
+        assert result == expected_result
+
+    def test_when_only_has_numbers(self):
+        # Arrange
+        trainer = TrainerReciver()
+        fake_data = [{"name":"11111"}, {"name": "22222"}]
+        expected_result = [{"name":"11111"}, {"name": "22222"}]
+
+        # ACT
+        result = trainer.capitalize(fake_data) # SUT (SUBJECT UNDER TEST)
+        
+        # ASSERT
+        assert result == expected_result
+
+    def test_when_start_with_number(self):
+        # Arrange
+        trainer = TrainerReciver()
+        fake_data = [{"name":"11alberto"}, {"name": "22silvia"}]
+        expected_result = [{"name":"11alberto"}, {"name": "22silvia"}]
+
+        # ACT
+        result = trainer.capitalize(fake_data) # SUT (SUBJECT UNDER TEST)
+        
+        # ASSERT
+        assert result == expected_result
+
+    def test_when_name_come_empty(self):
+        # Arrange
+        trainer = TrainerReciver()
+        fake_data = [{"name":""}, {"name": "22silvia"}]
+        expected_result = [{"name":""}, {"name": "22silvia"}]
 
         # ACT
         result = trainer.capitalize(fake_data) # SUT (SUBJECT UNDER TEST)
